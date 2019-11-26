2db5d99f01d9f3f74e8cb5d214525a35fd29d87b
Ajar-maharjan <49090416+Ajar-maharjan@users.noreply.github.com>
2019-11-26 19:17:01 +0545

    Final complete application push

--- app/src/main/java/com/novc/assignment1/MainActivity.java
+++ app/src/main/java/com/novc/assignment1/MainActivity.java
@@ -14,25 +14,23 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
-    TextView tvChecking, tvCheckingShow , tvCheckout , tvCheckoutShow,
-    tvTotal, tvVat, tvGrossTotal;
+    TextView tvChecking, tvCheckingShow, tvCheckout, tvCheckoutShow,
+            tvTotal, tvVat, tvGrossTotal;
     Spinner sRoomtype;
     EditText etAdult, etChildren, etRoom;
     Button btncalculate;
     AlertDialog.Builder builder;
 
-    private String[] Roomtype ={"Deluxe","Presidential","Premium"};
-    double total, grosstotal,vat, roomcost,noofday;
-    Date dateChecking ,dateCheckout;
+    private String[] Roomtype = {"Deluxe", "Presidential", "Premium"};
+    double total, grosstotal, vat, roomcost, noofday;
+    Date dateChecking, dateCheckout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,53 +72,52 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
         btncalculate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (TextUtils.isEmpty(etAdult.getText())){
+                if (TextUtils.isEmpty(etAdult.getText())) {
                     etAdult.setError("Enter number of adult");
                     return;
                 }
-                if (TextUtils.isEmpty(etChildren.getText())){
+                if (TextUtils.isEmpty(etChildren.getText())) {
                     etAdult.setError("Enter number of adult");
                     return;
                 }
-                if (TextUtils.isEmpty(etRoom.getText())){
+                if (TextUtils.isEmpty(etRoom.getText())) {
                     etAdult.setError("Enter number of adult");
                     return;
                 }
-                if (etAdult.getText().toString().equals("0")){
+                if (etAdult.getText().toString().equals("0")) {
                     etAdult.setError("Adult cannot be 0");
                     return;
                 }
-                if (etRoom.getText().toString().equals("0")){
+                if (etRoom.getText().toString().equals("0")) {
                     etRoom.setError("Room cannot be 0");
                     return;
                 }
-                 if (dateChecking.before(dateCheckout)) {
-                     noofday =  Double.parseDouble(CheckDate());
-                     CalculateGross();
-                 }
-                 else {
-                     dialogmsg();
-                     AlertDialog alert = builder.create();
-                     alert.show();
-                 }
+                if (dateChecking.before(dateCheckout)) {
+                    noofday = Double.parseDouble(CheckDate());
+                    CalculateGross();
+                } else {
+                    dialogmsg();
+                    AlertDialog alert = builder.create();
+                    alert.show();
+                }
             }
         });
 
     }
 
 
-    private void loadCheckingDate(){
+    private void loadCheckingDate() {
         final Calendar date1 = Calendar.getInstance();
         int year = date1.get(Calendar.YEAR);
         int month = date1.get(Calendar.MONTH);
         int day = date1.get(Calendar.DAY_OF_MONTH);
 
         DatePickerDialog datePickerChecking = new DatePickerDialog(
-                this,this,year,month,day);
+                this, this, year, month, day);
         datePickerChecking.show();
     }
 
-    private void loadCheckoutDate(){
+    private void loadCheckoutDate() {
         final Calendar date2 = Calendar.getInstance();
         int year = date2.get(Calendar.YEAR);
         int month = date2.get(Calendar.MONTH);
@@ -147,7 +144,7 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
     @Override
     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
         month = month + 1;
-        String date =  dayOfMonth + "-" + month + "-" + year;
+        String date = dayOfMonth + "-" + month + "-" + year;
         SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
         try {
             dateChecking = format.parse(date);
@@ -157,29 +154,27 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
         tvCheckingShow.setText(date);
     }
 
-    private void CalculateGross(){
+    private void CalculateGross() {
         if (sRoomtype.getSelectedItem().toString().equals("Deluxe")) {
             roomcost = 2000;
-        }
-        else if (sRoomtype.getSelectedItem().toString().equals("Presidential")) {
+        } else if (sRoomtype.getSelectedItem().toString().equals("Presidential")) {
             roomcost = 5000;
-        }
-        else if (sRoomtype.getSelectedItem().toString().equals("Premium")) {
+        } else if (sRoomtype.getSelectedItem().toString().equals("Premium")) {
             roomcost = 4000;
         }
         int noofroom = Integer.parseInt(etRoom.getText().toString());
         total = roomcost * noofroom * noofday;
-        vat = (0.13*total);
+        vat = (0.13 * total);
         grosstotal = total + vat;
         String setTotal = "Total : " + total;
-        String setVat ="Vat (13%) : " + vat;
-        String setGrossTotal = "Gross Total : "+ grosstotal;
+        String setVat = "Vat (13%) : " + vat;
+        String setGrossTotal = "Gross Total : " + grosstotal;
         tvTotal.setText(setTotal);
         tvVat.setText(setVat);
         tvGrossTotal.setText(setGrossTotal);
     }
 
-    private String CheckDate(){
+    private String CheckDate() {
         Calendar cCal = Calendar.getInstance();
         cCal.setTime(dateChecking);
         int cYear = cCal.get(Calendar.YEAR);
@@ -202,17 +197,17 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
 
         long diff = date2.getTimeInMillis() - date1.getTimeInMillis();
         float dayCount = (float) diff / (24 * 60 * 60 * 1000);
-        return dayCount + "" ;
+        return dayCount + "";
     }
 
-    private void dialogmsg(){
+    private void dialogmsg() {
         builder.setTitle("Date mismatch")
-            .setMessage("Invalid Checkout date. Checkout date cannot greater than checking date")
-            .setCancelable(false)
-            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int id) {
-                    dialog.cancel();
-                }
-            });
+                .setMessage("Invalid Checkout date. Checkout date cannot greater than checking date")
+                .setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
     }
 }
